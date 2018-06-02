57969a3f9888eee8cf2d84e60c6b4b3540374cfe
Simon Howey <simon.howey@gmail.com>
2018-06-02 21:18:16 +0000

    add single missing character

--- buildprod.sh
+++ buildprod.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Usage buildprod.sh [remotehost]
+echo "Remote URL is: ${1}"
 docker build --no-cache --build-arg remote_url=$1 ./build/ -t confwebbuild
 docker run --name temp-confwebbuild confwebbuild /bin/true
-rm -f ./services/nginx/www
+rm -rf ./services/nginx/www
 docker cp temp-confwebbuild:/usr/apprepo/www ./services/nginx/www
 docker rm temp-confwebbuild
 docker-compose -f ./services/docker-compose.prod.yml build
