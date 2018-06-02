16aa1c885bbf170aca59d059674ea9c2aa9caf31
Simon Howey <simon.howey@gmail.com>
2018-06-02 23:53:32 +0000

    update for client changes

--- buildprod.sh
+++ buildprod.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#Usage buildprod.sh [remotehost]
-echo "Remote URL is: ${1}"
-docker build --no-cache --build-arg remote_url=$1 ./build/ -t confwebbuild
+docker build --no-cache ./build/ -t confwebbuild
 docker run --name temp-confwebbuild confwebbuild /bin/true
 rm -rf ./services/nginx/www
 docker cp temp-confwebbuild:/usr/apprepo/www ./services/nginx/www
 docker rm temp-confwebbuild
+docker image rm confwebbuild:latest
 docker-compose -f ./services/docker-compose.prod.yml build
